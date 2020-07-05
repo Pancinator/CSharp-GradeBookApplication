6b03c1791e4934f99a376c119d40fa1c9d9765e8
H390882 <Matej.Pancak@honeywell.com>
2020-07-05 21:34:07 +0200

    Step three done

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,29 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Not enough students.");
+            }
+            if (averageGrade >= 80)
+            {
+                return 'A';
+            } else if (averageGrade >= 60 && averageGrade <= 80)
+            {
+                return 'B';
+            } else if (averageGrade >= 40 && averageGrade <= 60)
+            {
+                return 'C';
+            } else if (averageGrade >= 20 && averageGrade <= 40)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
